6502a81f2c2edc1a31a4e883ad55ee1d8ab60317
Alan Johnson <ajosephjohnson@gmail.com>
2021-01-10 17:53:18 -0500

    Welcome blurb small edits

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,35 +4,43 @@ import { Link } from 'gatsby';
 import CapitolIcon from './capitol-icon';
 
 const Header: FC<{ siteTitle: string }> = ({ siteTitle }) => (
-  <header className="h-20 border-b-2 flex pt-1">
-    <Link to="/" className="h-full flex items-center w-max p-3 hover:underline">
-      <CapitolIcon />
-      <h1 className="text-2xl ml-3">{siteTitle}</h1>
-    </Link>
-    <div className="w-full flex justify-center">
+  <>
+    <header className="h-20 border-b-2 flex pt-1">
       <Link
-        to="/organizations/"
-        activeStyle={{ textDecoration: 'underline' }}
-        className="hover:underline text-xl p-3 flex items-center"
+        to="/"
+        className="h-full flex items-center w-max p-3 hover:underline"
       >
-        <h2>Organizations</h2>
+        <CapitolIcon />
+        <h1 className="text-2xl ml-3">{siteTitle}</h1>
       </Link>
-      <Link
-        to="/books/"
-        activeStyle={{ textDecoration: 'underline' }}
-        className="hover:underline text-xl p-3 flex items-center"
-      >
-        <h2>Books</h2>
-      </Link>
-      <Link
-        to="/podcasts/"
-        activeStyle={{ textDecoration: 'underline' }}
-        className="hover:underline text-xl p-3 flex items-center"
-      >
-        <h2>Podcasts</h2>
-      </Link>
+      <div className="w-full flex justify-center">
+        <Link
+          to="/organizations/"
+          activeStyle={{ textDecoration: 'underline' }}
+          className="hover:underline text-xl p-3 flex items-center"
+        >
+          <h2>Organizations</h2>
+        </Link>
+        <Link
+          to="/books/"
+          activeStyle={{ textDecoration: 'underline' }}
+          className="hover:underline text-xl p-3 flex items-center"
+        >
+          <h2>Books</h2>
+        </Link>
+        <Link
+          to="/podcasts/"
+          activeStyle={{ textDecoration: 'underline' }}
+          className="hover:underline text-xl p-3 flex items-center"
+        >
+          <h2>Podcasts</h2>
+        </Link>
+      </div>
+    </header>
+    <div className="italic ml-4 mt-1.5">
+      Educational material on the topic of campaign finance reform
     </div>
-  </header>
+  </>
 );
 
 export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,29 +6,29 @@ import SEO from '../components/seo';
 const IndexPage: FC<{}> = () => (
   <Layout>
     <SEO title="Home" />
-    <div className="flex flex-col items-center justify-center max-w-md mx-auto my-7">
+    <div className="flex flex-col items-center justify-center max-w-md mx-auto my-7 text-base">
       <p>Hi, My name is Alan &#128075;</p>
       <br />
       <p>
         I&#39;m a software engineer who lives in Brooklyn, and I happen to be
-        very interested in the topic of how our elections are funded.
+        interested in the topic of how our elections are funded.
       </p>
       <br />
       <p>
         This website contains curated lists of reference and educational
-        material on the subject of campaign finance reform in a variety of
+        material on the subject of campaign finance reform through a variety of
         different mediums.
       </p>
       <br />
       <p>
         I hope you find some of this helpful and that we can continue to learn
         and, ultimately, fight together for the fundamental reform that our
-        country clearly needs.
+        country dearly needs.
       </p>
       <br />
       <p>
-        Know of a great resource that I am missing? Have a burning question?
-        Email me at{' '}
+        Know of a great resource that I am missing? Have a question burning a
+        hole in your brain? Reach me at{' '}
         <a
           className="underline"
           href="mailto:ajosephjohnson@gmail.com"
@@ -37,6 +37,7 @@ const IndexPage: FC<{}> = () => (
         >
           ajosephjohnson@gmail.com
         </a>
+        .
       </p>
       <br />
       <p>Hope you enjoy!</p>
